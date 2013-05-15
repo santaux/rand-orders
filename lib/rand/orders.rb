d4b5edf0c05d0d2bb1f57cca681fc3f313a34613
santaux <santaux@gmail.com>
2013-05-15 22:32:23 +0400

    Changed documentation to yardoc correct format

--- lib/rand/orders.rb
+++ lib/rand/orders.rb
@@ -1,71 +1,46 @@
 module Rand
   module Orders
     module Relation
-      ##
       # Returns randomly sorted records from database.
       #
-      # == Returns:
-      # ActiveRecord::Relation object with order(...) statement.
-      # == Example:
-      # Item.where(:user_id => 1).random
+      # @example Item.where(:user_id => 1).random
       def random
         relation = clone
         relation.order_values += [random_clause]
         relation
       end
 
-      ##
       # Returns randomly sorted records from database with field value
       # in specified range.
       #
-      # == Parameters:
-      # field_name::
-      #   A name of the field
-      # range::
-      #   Range object with MIN and MAX values for specified field.
+      # @param [String, Symbol] field_name a name of the field
+      # @param [Range] range range object with MIN and MAX values for specified field.
       #
-      # == Returns:
-      # ActiveRecord::Relation object with order(...) and 
-      # where("#{field_name} > #{range.min} AND #{field_name} < #{range.max}") statements
-      # == Example:
-      # Item.random_in_range(:amount, (2..5)).limit(10)
+      # @example Item.random_in_range(:amount, (2..5)).limit(10)
       def random_in_range(field_name, range)
         relation = random
         relation.where_values += build_where("#{field_name} > #{range.min} AND #{field_name} < #{range.max}", [])
         relation
       end
 
-      ##
       # Returns randomly sorted records from database with field value
       # equal to specified parameter.
       #
-      # == Parameters:
-      # field_name::
-      #   A name of the field or range comparing.
-      # value::
-      #   A value of the specified field.
+      # @param [String, Symbol] field_name a name of the field or range comparing.
+      # @param value a value of the specified field.
       #
-      # == Returns:
-      # ActiveRecord::Relation object with order(...) and where(field_name => value) statements
-      # == Example:
-      # Item.random_equal(:user_id, 1)
+      # @example Item.random_equal(:user_id, 1)
       def random_equal(field_name, value)
         relation = random
         relation.where_values += build_where({field_name.to_sym => value}, [])
         relation
       end
 
-      ##
       # Returns randomly sorted array of specified field values.
       #
-      # == Parameters:
-      # field_name::
-      #   A name of the field to return.
+      # @param [String, Symbol] field_name a name of the field to return
       #
-      # == Returns:
-      # Array of the specified field values.
-      # == Example:
-      # Item.select_random(:amount)
+      # @example Item.select_random(:amount)
       def select_random(field_name)
         random.pluck(field_name)
       end
